2a9ec06a4f4ea41c61ce4e58a501079733b9925a
Lucas ABM <lucasabmacedo@hotmail.com>
2021-04-21 21:13:45 -0300

    Reverte principal

--- JoaoAtacadao/src/telas/Principal.java
+++ JoaoAtacadao/src/telas/Principal.java
@@ -5,8 +5,6 @@
  */
 package telas;
 
-import camadaDePersistencia.Conexao;
-
 /**
  *
  * @author João Víctor
@@ -18,9 +16,6 @@ public class Principal extends javax.swing.JFrame {
      */
     public Principal() {
         initComponents();
-        botaoMagico.setOpaque(false);
-        botaoMagico.setContentAreaFilled(false);
-        botaoMagico.setBorderPainted(false);
         setResizable(false); // maximize button disable
     }
 
@@ -41,7 +36,6 @@ public class Principal extends javax.swing.JFrame {
         jButton5 = new javax.swing.JButton();
         jButton6 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
-        botaoMagico = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("João Atacadão");
@@ -113,13 +107,6 @@ public class Principal extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(102, 0, 255));
         jLabel1.setText("Bem-vindo ao João Atacadão");
 
-        botaoMagico.setMnemonic('M');
-        botaoMagico.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                botaoMagicoActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
@@ -140,21 +127,14 @@ public class Principal extends javax.swing.JFrame {
                 .addContainerGap(44, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(botaoMagico)
-                .addGap(41, 41, 41)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 475, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(141, 141, 141))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(26, 26, 26)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(43, 43, 43)
-                        .addComponent(botaoMagico)))
+                .addGap(26, 26, 26)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 75, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(37, 37, 37)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -214,44 +194,6 @@ public class Principal extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_jButton6ActionPerformed
 
-    private void botaoMagicoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_botaoMagicoActionPerformed
-        Conexao.create("eletroeletronico", "'1', 'Geladeira', 1000, 'eletrolux', 'eletroeletronico'", "'1', '1000', '500', true");
-        Conexao.create("eletroeletronico", "'2', 'Microondas', 500, 'panasonic', 'eletroeletronico'", "'2', '2000', '300', false");
-        Conexao.create("eletroeletronico", "'3', 'Cafeteira', 200, 'tres coracoes', 'eletroeletronico'", "'3', '100', '900', true");
-        
-        Conexao.create("vestuario", "'4', 'Salto Preto', 200, 'havan', 'vestuario'", "'4', '37', 'preto', 'plastico'");
-        Conexao.create("vestuario", "'5', 'Sapatilha Cinderela', 750, 'disney', 'vestuario'", "'5', '33', 'azul-transparente', 'cristal'");
-        Conexao.create("vestuario", "'6', 'Casaco Branco', 270, 'zara', 'vestuario'", "'6', 'L', 'branco', 'tecido'");
-        
-        Conexao.create("computador", "'7', 'Laptop Xuxa', 100, 'ciatoy', 'computador'", "'7', 'nao', 'nao', true, '10kb', 'nao', 'rosa', 'Xuxa So Para Baixinhos'");
-        Conexao.create("computador", "'8', 'Pichau Gamer', 7000, 'pichau', 'computador'", "'8', 'i9-11', 'rtx3090', true, '64gb', 'boazona', 'todas', 'XGamerDestroyer'");
-        Conexao.create("computador", "'9', 'PC Generico', 1300, 'lenovo', 'computador'", "'9', 'i3-10', 'onboard', false, '8gb', 'media', 'azul', 'home office'");
-        
-        Conexao.create("celular", "'10', 'Samsung S20', 3000, 'Samsung', 'celular'", "'10', 'snapdragon', '15', '16gb', '65mp', 'cinza', 'S20'");
-        Conexao.create("celular", "'11', 'Moto G8', 1300, 'Motorola', 'celular'", "'11', 'snapdragon', '13', '8gb', '32mp', 'preto', 'G8'");
-        Conexao.create("celular", "'12', 'Nokia 3180', 200, 'Nokia', 'celular'", "'12', 'uns fios', '5', '16kb', 'nao', 'azul', '3180'");
-        
-        Conexao.create("periferico", "'13', 'HyperX Surge', 300, 'HyperX', 'periferico'", "'13', 'mouse', 'usb', 'preto', 'Surge'");
-        Conexao.create("periferico", "'14', 'JBL Quantum 100', 200, 'JBL', 'periferico'", "'14', 'headset', 'p3', 'preto', 'Quantum 100'");
-        Conexao.create("periferico", "'15', 'Monitor Samsung', 600, 'Samsung', 'periferico'", "'15', 'monitor', 'hdmi', 'preto', 'generico'");
-        
-        Conexao.create("filme", "'16', 'Interestelar', 50, 'Paramount', 'filme'", "'16', 'nolan', 180, '14', 'scifi'");
-        Conexao.create("filme", "'17', 'Os Incriveis', 20, 'Disney', 'filme'", "'17', 'roberta', 90, 'L', 'herois'");
-        Conexao.create("filme", "'18', 'Madagascar', 15, 'Pixar', 'filme'", "'18', 'caio', 99, 'L', 'aventura'");
-        
-        Conexao.create("livro", "'19', 'Fundacao', 20, 'Aleph', 'livro'", "'19', 'asimov', 180, 'aleph', '14156671', '14', 'scifi'");
-        Conexao.create("livro", "'20', 'Senhor dos Aneis', 40, 'IMFE', 'livro'", "'20', 'tolkien', 310, 'IMFE', '416561', '12', 'aventura'");
-        Conexao.create("livro", "'21', 'Duna', 50, 'Editora', 'livro'", "'21', 'frank herbert', 680, 'Editora', '3315517', '14', 'scifi'");
-        
-        Conexao.create("funcionario", "'062.942.051-31', 'Lucas ADM', '2000-10-02', 7000, true, '3591'");
-        Conexao.create("funcionario", "'415.941.131-13', 'Joao Pedro', '2001-05-02', 3000, false, null");
-        Conexao.create("funcionario", "'516.521.331-22', 'Joao Victor', '2000-11-13', 3000, false, null");
-        
-        Conexao.create("cliente", "'145.132.672-13', 'Joao Bernardo', '1973-10-11', 200, '123'");
-        Conexao.create("cliente", "'613.754.356-11', 'Joao Lucas', '2013-10-11', 1000, '123'");
-        Conexao.create("cliente", "'854.469.345-77', 'Joao Maranhão', '2001-09-01', 2200, '123'");
-    }//GEN-LAST:event_botaoMagicoActionPerformed
-
     /**
      * @param args the command line arguments
      */
@@ -288,7 +230,6 @@ public class Principal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton botaoMagico;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
